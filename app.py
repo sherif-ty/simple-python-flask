fc70f51682356545583a70fce1eed56c1a12f632
EC2 Default User <ec2-user@ip-172-31-78-31.ec2.internal>
2024-12-10 11:16:12 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello, CICD, Change number 0 "
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
