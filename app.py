b2b180592faf2d34e74e77915de005030a10b20f
sherif-abdelkader1_voda <sherif.abdelkader1@vodafone.com>
2024-12-11 15:49:26 +0200

    5

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, CICD, Change number 4 "
+    return "Hello, CICD, Change number 5 "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
